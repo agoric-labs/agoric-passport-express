599b0dc7e5d55736247d136044e157cb591d0a36
Dan Connolly <dckc@madmode.com>
2023-12-04 00:20:40 -0600

    feat: discord auth (WIP)
     - login view: /auth/discord link
     - routes: /auth/discord, /auth/discord/redirect
       - req.logout() takes 0 params???
     - passport-discord dep (w/types)
     - config: DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRETa
     - models: provider field, discordId, ...

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,7 +1,9 @@
 import passport from 'passport';
 import passportGoogle from 'passport-google-oauth20';
+import { Strategy as DiscordStrategy } from 'passport-discord';
 import User from '../models/User';
 import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '../utils/secrets';
+import { DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET } from '../utils/secrets';
 const GoogleStrategy = passportGoogle.Strategy;
 
 passport.serializeUser((user, done) => {
@@ -26,10 +28,44 @@ passport.use(
       // If user doesn't exist creates a new user. (similar to sign up)
       if (!user) {
         const newUser = await User.create({
+          provider: 'google',
           googleId: profile.id,
           username: profile.displayName,
           email: profile.emails?.[0].value,
         });
+        if (newUser) {
+          done(null, newUser);
+        }
+      } else {
+        done(null, user);
+      }
+    }
+  )
+);
+
+passport.use(
+  new DiscordStrategy(
+    {
+      clientID: DISCORD_CLIENT_ID,
+      clientSecret: DISCORD_CLIENT_SECRET,
+      callbackURL: '/auth/discord/redirect',
+      scope: ['identify', 'guilds'],
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      console.log('discord profile', profile);
+
+      const user = await User.findOne({ discordeId: profile.id });
+
+      // If user doesn't exist creates a new user. (similar to sign up)
+      if (!user) {
+        const newUser = await User.create({
+          provider: 'discord',
+          discordeId: profile.id,
+          avatar: profile.avatar,
+          username: profile.username,
+          fetchedAt: profile.fetchedAt,
+        });
+        console.log('@@@', { newUser });
         if (newUser) {
           done(null, newUser);
         }
